7699ef21b22ea922da0260cec7409ea54ec224be
satoru <calendariosusfterravermelha@gmail.com>
2024-08-26 18:59:19 -0300

    criação de pasta com arquivos utilizados para automação da remoção de pacientes das equipes do upcities, sistema de agendamento online de consultas da prefeitura de Vila Velha

---
+++ auto-exclusao/automacao01.py
@@ -0,0 +1,50 @@
+import pyautogui as pag
+import time
+
+def clicar_em_coord(coord):
+    pag.moveTo(coord)
+    pag.click()
+
+def tabs(a):
+   for i in range(a):
+    pag.press('tab')
+
+
+coord_chrome = (3049, 215)
+
+coord_select_us = 15
+coord_btn_visualizar = 6
+coord_btn_editar = 13
+coord_btn_excluir = 10
+
+# coord_select_us = (2408, 464)
+# coord_btn_visualizar = (3697, 633)
+# coord_btn_editar = (3698, 248)
+# coord_btn_excluir = (3576, 252)
+
+contador = 0
+limite = 9
+
+clicar_em_coord(coord_chrome)
+
+while contador < limite: #inserir a quantidade de registros  
+  pag.press('f5')
+  time.sleep(5)
+  tabs(coord_select_us)#inserir coord do select da Unidade
+  pag.write('terra')
+  pag.press('enter')
+  time.sleep(3)
+  pag.press('tab')
+  pag.write('rural')#nome da equipe
+  pag.press('enter')
+  time.sleep(3)
+  tabs(coord_btn_visualizar)#inserir coord do btn do olho de visualizar
+  pag.press('enter')
+  time.sleep(3)
+  tabs(coord_btn_editar)#inserir coord do icone do btn Editar
+  pag.press('enter')
+  time.sleep(3)
+  tabs(coord_btn_excluir)#inserir coord do icone do btn excluir
+  pag.press('enter')
+  time.sleep(3)
+  contador += 1
---
+++ auto-exclusao/get_coordenadas.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+try:
+    while True:
+        x, y = pyautogui.position()  # Obtém as coordenadas X e Y do mouse
+        print(f'X: {x}, Y: {y}', end='\r')  # Imprime as coordenadas no console
+        time.sleep(0.1)  # Atraso de 0,1 segundo para evitar sobrecarga de CPU
+except KeyboardInterrupt:
+    print("\nPrograma encerrado.")
